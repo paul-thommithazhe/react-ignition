868a117bc208626dfff2d03b158b86b488584a81
thushadt <thushkply@gmail.com>
2025-06-20 16:35:54 +0530

    top-rated-rest

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,12 +1,24 @@
 import RestaurantCard from "./Restaurent";
 import resObj from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
+  const [listOfResObj, setListOfResObj] = useState(resObj);
   return (
     <div className="body">
-      <div className="search">search</div>
+      <div className="filter-btn">
+        <button
+          onClick={() => {
+            setListOfResObj(
+              listOfResObj.filter((resData) => resData.info.avgRating > 4.6)
+            );
+          }}
+        >
+          Top Rated Restaurents
+        </button>
+      </div>
       <div className="restaurant-container">
-        {resObj.map((restaurants, index) => (
+        {listOfResObj.map((restaurants, index) => (
           <RestaurantCard key={restaurants.info.id} resData={restaurants} />
         ))}
       </div>
