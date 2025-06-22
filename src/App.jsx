cf353254696df9a15c3cc19d929fe889a73c4c51
paul-thommithazhe <thushkply@gmail.com>
2025-06-22 21:07:52 +0530

    feat:lazy-loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-import About from "./components/about";
 import Contact from "./components/contact";
 import Error from "./components/error";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import { lazy, Suspense } from "react";
 
 //Body
+
+//lazy loading // Dynamic bundling // code splitting // chunking.....
+
+const About = lazy(() => import("./components/about"));
 
 const AppLayout = () => {
   return (
@@ -24,7 +28,14 @@ const appRouter = createBrowserRouter([
     errorElement: <Error />,
     children: [
       { path: "/", element: <Body /> },
-      { path: "/about", element: <About /> },
+      {
+        path: "/about",
+        element: (
+          <Suspense fallback={<h1> Loading ....</h1>}>
+            <About />
+          </Suspense>
+        ),
+      },
       { path: "/contact", element: <Contact /> },
     ],
   },
