69727b9b4c4e87d3b9f854c8b124863ef7b9b9a2
paul-thommithazhe <thushkply@gmail.com>
2025-06-22 13:05:16 +0530

    added:onine-status

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
 import { LOGO_URL } from "../utils/constants";
 import { Link } from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus";
 const Header = () => {
+  const onlineStatus = useOnlineStatus();
   return (
     <div className="main-header">
       <div className="logo-container">
@@ -8,8 +10,8 @@ const Header = () => {
       </div>
       <div className="nav-items">
         <ul>
+          <li>Online Status: {onlineStatus ? "✅" : "🔴"}</li>
           <li>
-            {" "}
             <Link to="/">Home </Link>
           </li>
           <li>
